f7d5c0c9bb05ba9767ff9b09e92c1fe3c8c4df00
phongbj <20520364@gm.uit.edu.vn>
2022-05-07 11:49:51 +0700

    add mon an

---
+++ WeddingApp/WeddingApp/ViewModels/DishListVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WeddingApp.Views;
+using WeddingApp.Views.UserControls.Admin;
+
+namespace WeddingApp.ViewModels
+{
+    class DishListVM : ViewModelBase
+    {
+        public ICommand OpenAddDishListWindowCommand { get; set; }
+
+        public DishListVM ()
+        {
+            OpenAddDishListWindowCommand = new RelayCommand<DishListUC>((parameter) => { return true; }, (parameter) => OpenAddDishListWindow(parameter));
+        }
+
+        public void OpenAddDishListWindow(DishListUC parameter)
+        {
+            AddDishWindow addDishListWindow = new AddDishWindow();
+            addDishListWindow.ShowDialog();
+        }
+    }
+}
