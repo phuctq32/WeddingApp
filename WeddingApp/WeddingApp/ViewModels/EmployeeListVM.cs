a2014be5e29d53d6c881ff21c7d97d19a9957b1f
Đam Sa <20521154@ms.uit.edu.vn>
2022-05-07 11:06:17 +0700

    add OpenAddEmployeeWindowCommand

--- WeddingApp/WeddingApp/ViewModels/EmployeeListVM.cs
+++ WeddingApp/WeddingApp/ViewModels/EmployeeListVM.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WeddingApp.Views;
+using WeddingApp.Views.UserControls.Admin;
 
 namespace WeddingApp.ViewModels
 {
     class EmployeeListVM : ViewModelBase
     {
+        public ICommand OpenAddEmployeeWindowCommand { get; set; }
 
+        public EmployeeListVM()
+        {
+            OpenAddEmployeeWindowCommand = new RelayCommand<EmployeeListUC>((parameter) => { return true; }, (parameter) => OpenAddEmployee(parameter));
+        }
 
-
+        public void OpenAddEmployee(EmployeeListUC parameter)
+        {
+            AddEmployeeWindow addEmployeeWindow = new AddEmployeeWindow();
+            addEmployeeWindow.ShowDialog();
+        }
     }
 }
