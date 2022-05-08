243603af347d2ec08d404666332b168612868e34
Điền Trương Kim <20520442@ms.uit.edu.vn>
2022-05-08 20:19:58 +0700

    AddHallType

---
+++ WeddingApp/WeddingApp/ViewModels/AddHallTypeVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WeddingApp.Views;
+using WeddingApp.Models;
+
+namespace WeddingApp.ViewModels
+{
+    internal class AddHallTypeVM
+    {
+        public AddHallTypeVM()
+        {
+
+        }
+    }
+}
