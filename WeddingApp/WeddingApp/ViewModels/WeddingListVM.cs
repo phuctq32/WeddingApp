1dd1a5c85c81658a807044ae736b4a5608096f1b
Điền Trương Kim <20520442@ms.uit.edu.vn>
2022-05-08 18:23:59 +0700

    WeddingList 1.0

---
+++ WeddingApp/WeddingApp/ViewModels/WeddingListVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeddingApp.Models;
+using WeddingApp.Views.UserControls;
+using System.Windows.Input;
+
+namespace WeddingApp.ViewModels
+{
+    internal class WeddingListVM
+    {
+        public ICommand LoadedCommand { get; set; }
+
+        public WeddingListVM()
+        {
+            LoadedCommand = new RelayCommand<WeddingListUC>(parameter => true, parameter => Load(parameter));
+        }
+        public void Load(WeddingListUC weddingListUC)
+        {
+            List<WEDDING> wEDDINGs = new List<WEDDING>();
+            wEDDINGs = Data.Ins.DB.WEDDINGs.ToList();
+            weddingListUC.weddingList.ItemsSource = wEDDINGs;
+        }
+    }
+}
