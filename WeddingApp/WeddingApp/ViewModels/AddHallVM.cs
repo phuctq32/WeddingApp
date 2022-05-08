dc785d94faa0b88664b71e511085667510aab9bd
Điền Trương Kim <20520442@ms.uit.edu.vn>
2022-05-08 20:46:50 +0700

    AddHallUC

---
+++ WeddingApp/WeddingApp/ViewModels/AddHallVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WeddingApp.Views;
+using WeddingApp.Models;
+
+namespace WeddingApp.ViewModels
+{
+    internal class AddHallVM
+    {
+        public ICommand LoadedCommand { get; set; }
+
+        public AddHallVM()
+        {
+            LoadedCommand = new RelayCommand<AddWeddingHallWindow>(parameter => true, parameter => Load(parameter));
+        }
+        public void Load(AddWeddingHallWindow addWeddingHallWindow)
+        {
+            List<BALLROOMTYPE> list = Data.Ins.DB.BALLROOMTYPEs.ToList();
+            foreach (var item in list)
+            {
+                addWeddingHallWindow.comboBoxType.Items.Add(item.TYPENAME);
+            }
+
+        }
+    }
+}
