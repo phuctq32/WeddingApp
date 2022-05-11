3bd7a4639db0794eda68930735ed3f4f5430366e
Điền Trương Kim <20520442@ms.uit.edu.vn>
2022-05-11 15:53:41 +0700

    InvoiceListVM

--- WeddingApp/WeddingApp/ViewModels/InvoiceListVM.cs
+++ WeddingApp/WeddingApp/ViewModels/InvoiceListVM.cs
@@ -6,24 +6,43 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using WeddingApp.Views.UserControls;
 using WeddingApp.Models;
+using System.Windows.Controls;
 
 namespace WeddingApp.ViewModels
 {
-    internal class InvoiceListVM
+    internal class InvoiceListVM : ViewModelBase
     {
         public ICommand LoadedCommand { get; set; }
+        public ICommand PayCommand  { get; set; }
+        private List<INVOICE> listInvoice;
+
+        public List<INVOICE> ListInvoice
+        {
+            get => listInvoice;
+            set
+            {
+                listInvoice = value;
+                OnPropertyChanged("Status");
+                OnPropertyChanged("ListInvoice");
+            }
+        }
 
         public InvoiceListVM()
         {
             LoadedCommand = new RelayCommand<InvoiceListUC>(parameter => true, parameter => Loaded(parameter));
+            PayCommand = new RelayCommand<ListViewItem>(parameter => true, parameter => Paid(parameter));
+
         }
         public void Loaded(InvoiceListUC invoiceListUC)
         {
-            List<INVOICE> iNVOICEs = new List<INVOICE>();
-            iNVOICEs = Data.Ins.DB.INVOICES.ToList();
-            invoiceListUC.invoiceList.ItemsSource = iNVOICEs; 
+            ListInvoice = Data.Ins.DB.INVOICES.Where(x=>x.STATUS == 1).ToList();
         }
-
-
+        public void Paid(ListViewItem listViewItem)
+        {
+            INVOICE paidInvoice = listViewItem.DataContext as INVOICE;
+            paidInvoice.STATUS = 2;
+            Data.Ins.DB.SaveChanges();
+            ListInvoice.Clear();
+        }
     }
 }
