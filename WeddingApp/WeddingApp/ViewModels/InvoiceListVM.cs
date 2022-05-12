0fdb2ce23cb83762c1f277fcca62ad9159d44272
Điền Trương Kim <20520442@ms.uit.edu.vn>
2022-05-12 15:40:34 +0700

    update InvoiceListVM

--- WeddingApp/WeddingApp/ViewModels/InvoiceListVM.cs
+++ WeddingApp/WeddingApp/ViewModels/InvoiceListVM.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using WeddingApp.Views.UserControls;
 using WeddingApp.Models;
+using WeddingApp.Views;
 using System.Windows.Controls;
+using System.Globalization;
 
 namespace WeddingApp.ViewModels
 {
@@ -39,9 +41,22 @@ namespace WeddingApp.ViewModels
         }
         public void Paid(ListViewItem listViewItem)
         {
+            CultureInfo cultureInfo = CultureInfo.GetCultureInfo("vi-VN");
             INVOICE paidInvoice = listViewItem.DataContext as INVOICE;
-            paidInvoice.STATUS = 2;
-            Data.Ins.DB.SaveChanges();
+            InvoiceWindow invoiceWindow = new InvoiceWindow();
+            List<SERVE> listServe = Data.Ins.DB.SERVEs.Where(x=>x.WEDDINGID == paidInvoice.WEDDINGID).ToList();
+            invoiceWindow.listView.ItemsSource = listServe;
+            invoiceWindow.txtGroom.Text = paidInvoice.WEDDING.GROOM;
+            invoiceWindow.txtBride.Text = paidInvoice.WEDDING.BRIDE;
+            invoiceWindow.txtDeposit.Text = paidInvoice.WEDDING.DEPOSIT.ToString("C0", cultureInfo);
+            invoiceWindow.txtRemaining.Text = paidInvoice.REMAININGCOST.ToString("C0", cultureInfo);
+            invoiceWindow.txtTableAmount.Text = paidInvoice.WEDDING.TABLEAMOUNT.ToString();
+            invoiceWindow.txtTableCost.Text = paidInvoice.WEDDINGCOST.ToString("C0", cultureInfo);
+            invoiceWindow.txtTotalCost.Text = paidInvoice.TOTALCOST.ToString("C0", cultureInfo);
+            invoiceWindow.txtTotalServiceCost.Text = paidInvoice.SERVICECOST.ToString("C0", cultureInfo);
+            invoiceWindow.txtWeddingCost.Text = paidInvoice.WEDDINGCOST.ToString("C0", cultureInfo);
+            invoiceWindow.txtWeddingDate.Text = paidInvoice.WEDDING.WEDDINGDATE.ToString("dd/MM/yyyy");
+            invoiceWindow.ShowDialog();
             ListInvoice.Clear();
         }
     }
---
+++ WeddingApp/WeddingApp/ViewModels/InvoiceVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using System.Text;
+using System.Threading.Tasks;
+using WeddingApp.Models;
+using WeddingApp.Views;
+
+namespace WeddingApp.ViewModels
+{
+    internal class InvoiceVM:ViewModelBase
+    {
+        public Icommand
+    }
+}
