72aaf7228a7998612f9724a3c4c5d6ac37a6cf9f
Phuc Tran <tr.ph3322@gmail.com>
2022-05-16 17:39:38 +0700

    completed 3 UC set wed info

---
+++ WeddingApp/WeddingApp/ViewModels/SetWeddingInfoVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WeddingApp.Views.UserControls;
+
+namespace WeddingApp.ViewModels
+{
+    internal class SetWeddingInfoVM : ViewModelBase
+    {
+        private SetWeddingInfomationUC SetWeddingInfoUserControl { get; set; }
+        private MenuUC MenuUserControl { get; set; }
+        private ServiceSelectionUC ServiceSelectionUserControl { get; set; } 
+        public ICommand SwitchUCCommand { get; set; }
+        public SetWeddingInfoVM() 
+        {
+            SetWeddingInfoUserControl = new SetWeddingInfomationUC();
+            MenuUserControl = new MenuUC();
+            ServiceSelectionUserControl = new ServiceSelectionUC();
+            SwitchUCCommand = new RelayCommand<SetWeddingInfomationUC>(p => true, p => SwitchUC(p));
+        }
+
+        private void SwitchUC(SetWeddingInfomationUC parameter)
+        {
+            
+        }
+    }
+}
