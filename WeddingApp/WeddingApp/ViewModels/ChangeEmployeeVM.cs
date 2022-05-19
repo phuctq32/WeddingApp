f73f79a2a3b06fb9c85630cbb5dd55ded416c347
Đam Sa <20521154@ms.uit.edu.vn>
2022-05-19 14:19:02 +0700

    LoadChangeInfo

---
+++ WeddingApp/WeddingApp/ViewModels/ChangeEmployeeVM.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using WeddingApp.Models;
+using WeddingApp.Views;
+using WeddingApp.Views.UserControls.Admin;
+
+namespace WeddingApp.ViewModels
+{
+    class ChangeEmployeeVM : ViewModelBase
+    {
+        public ICommand ChangeEmployeeCommand { get; set; }
+
+        public ICommand PasswordChangedCommand { get; set; }
+        public ICommand RePasswordChangedCommand { get; set; }
+
+        private string employeeName;
+
+        public string EmployeeName
+        { get => employeeName; set { employeeName = value; OnPropertyChanged(); } }
+
+        private string roleid;
+
+        public string Roleid { get => roleid; set { roleid = value; OnPropertyChanged(); } }
+
+        private string password;
+
+        public string Password
+        { get => password; set { password = value; OnPropertyChanged(); } }
+
+
+        private string rePassword;
+
+        public string RePassword
+        { get => rePassword; set { rePassword = value; OnPropertyChanged(); } }
+
+        private int salary;
+
+        public int Salary
+        { get => salary; set { salary = value; OnPropertyChanged(); } }
+
+        private DateTime date = DateTime.Now;
+
+        public DateTime Date
+        {
+            get => date; set { date = value; OnPropertyChanged(); }
+        }
+        public ChangeEmployeeVM()
+        {
+            
+            ChangeEmployeeCommand = new RelayCommand<ChangeEmployeeInformationWindow>((parameter) => { return true; }, (parameter) => SaveChangesEmployee(parameter));
+            PasswordChangedCommand = new RelayCommand<PasswordBox>((parameter) => { return true; }, (parameter) => { Password = parameter.Password; });
+            RePasswordChangedCommand = new RelayCommand<PasswordBox>((parameter) => { return true; }, (parameter) => { RePassword = parameter.Password; });
+
+        }
+
+        public void SaveChangesEmployee(ChangeEmployeeInformationWindow parameter)
+        {
+
+            CustomMessageBox.Show("Thay đổi thành công", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            // Check NAME
+            if (string.IsNullOrEmpty(parameter.txtEmployeeName.Text))
+            {
+                parameter.txtEmployeeName.Focus();
+                CustomMessageBox.Show("Họ và tên đang trống!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+
+            //Check Password
+            if (string.IsNullOrEmpty(parameter.PasswordBox.Password))
+            {
+                parameter.PasswordBox.Focus();
+                parameter.PasswordBox.Password = "";
+                CustomMessageBox.Show("Mật khẩu trống", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if (parameter.PasswordBox.Password.Contains(" "))
+            {
+                parameter.PasswordBox.Focus();
+                CustomMessageBox.Show("Mật khẩu không được chứa hoảng trắng!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            //check repassword
+            if (string.IsNullOrEmpty(parameter.RePasswordBox.Password))
+            {
+                parameter.RePasswordBox.Focus();
+                parameter.RePasswordBox.Password = "";
+                CustomMessageBox.Show("Chưa xác nhận mật khẩu", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if (parameter.RePasswordBox.Password.Contains(" "))
+            {
+                parameter.RePasswordBox.Focus();
+                CustomMessageBox.Show("Nhập lại mật khẩu không đúng", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (Password != RePassword)
+            {
+                parameter.RePasswordBox.Focus();
+                CustomMessageBox.Show("Nhập lại mật khẩu không đúng!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            // Check SALARY
+            if (string.IsNullOrEmpty(parameter.txtSalary.Text))
+            {
+                parameter.txtSalary.Focus();
+                CustomMessageBox.Show("Lương đang trống!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if (!Regex.IsMatch(parameter.txtSalary.Text, @"^[0-9_]+$"))
+            {
+                parameter.txtSalary.Focus();
+                CustomMessageBox.Show("Lương chỉ được nhập số!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            CustomMessageBox.Show("Thay đổi thành công", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            /*//setInfo after check
+
+            CurrentAccount.Employee.EMPLOYEENAME = parameter.txtEmployeeName.Text.Trim();
+            //CurrentAccount.Employee.ROLEID = parameter.txtPhone.Text.Trim();
+            CurrentAccount.Employee.PASSWORD = password.Trim();
+            CurrentAccount.Employee.SALARY = Salary;
+            try
+            {
+                //try to update database
+                Data.Ins.DB.SaveChanges();
+                CustomMessageBox.Show("Thay đổi thành công", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch
+            {
+                CustomMessageBox.Show("Thay đổi không thành công", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            parameter.Close();*/
+        }
+    }
+    
+}
--- WeddingApp/WeddingApp/ViewModels/EmployeeListVM.cs
+++ WeddingApp/WeddingApp/ViewModels/EmployeeListVM.cs
@@ -15,18 +15,41 @@ namespace WeddingApp.ViewModels
 {
     class EmployeeListVM : ViewModelBase
     {
+        public ICommand LoadedCommand { get; set; }
         public ICommand OpenAddEmployeeWindowCommand { get; set; }
-       
+        public ICommand OpenChangeEmployeeInformationWindowCommand { get; set; }
 
+        private List<EMPLOYEE> listEmployee;
+        public List<EMPLOYEE> ListEmployee
+        {
+            get => listEmployee;
+            set
+            {
+                listEmployee = value;
+                OnPropertyChanged("ListEmployee");
+            }
+        }
         public EmployeeListVM()
         {
+            LoadedCommand = new RelayCommand<EmployeeListUC>((parameter) => { return true; }, (parameter) => load(parameter));
             OpenAddEmployeeWindowCommand = new RelayCommand<EmployeeListUC>((parameter) => { return true; }, (parameter) => OpenAddEmployee(parameter));
+            OpenChangeEmployeeInformationWindowCommand = new RelayCommand<EmployeeListUC>((parameter) => { return true; }, (parameter) => OpenChangeEmployee(parameter));
+        }
+        public void load(EmployeeListUC parameter)
+        {
+            ListEmployee = Data.Ins.DB.EMPLOYEES.ToList();
+            parameter.ListView.ItemsSource = ListEmployee;
         }
 
         public void OpenAddEmployee(EmployeeListUC parameter)
         {
             AddEmployeeWindow addEmployeeWindow = new AddEmployeeWindow();
             addEmployeeWindow.ShowDialog();
+        } 
+        public void OpenChangeEmployee(EmployeeListUC parameter)
+        {
+            ChangeEmployeeInformationWindow changeEmployeeWindow = new ChangeEmployeeInformationWindow();
+            changeEmployeeWindow.ShowDialog();
         } 
        
     }
