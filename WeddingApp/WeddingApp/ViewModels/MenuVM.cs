e9b9437e2a902a6025083a7317eeff5962c5a6e3
Điền Trương Kim <20520442@ms.uit.edu.vn>
2022-05-30 20:15:44 +0700

    MenuVM 1.1

--- WeddingApp/WeddingApp/ViewModels/MenuVM.cs
+++ WeddingApp/WeddingApp/ViewModels/MenuVM.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using WeddingApp.Models;
 using WeddingApp.Views.UserControls;
 using WeddingApp.Views;
+using System.Windows.Controls;
 
 namespace WeddingApp.ViewModels
 {
@@ -14,18 +15,37 @@ namespace WeddingApp.ViewModels
     {
         public ICommand LoadedCommand { get; set; }
         public ICommand PreviousUCCommand { get; set; }
-        public ICommand NextUCCommand { get;}
+        public ICommand NextUCCommand { get; set; }
+        public ICommand AddToCartCommand { get; set; }
+        private long totalPrice;
+        public long TotalPrice
+        {
+            get => totalPrice;
+            set
+            {
+                totalPrice = value;
+                OnPropertyChanged("TotalPrice");
+            }
+        }
+        public MenuUC thisUC;
 
         public MenuVM()
         {
             LoadedCommand = new RelayCommand<MenuUC>(p => true, p => Loaded(p));
             PreviousUCCommand = new RelayCommand<MenuUC>(p => true, p => PreviousUC(p));
             NextUCCommand = new RelayCommand<MenuUC>(p => true, p => NextUC(p));
+            AddToCartCommand = new RelayCommand<ListViewItem>(p => true, p => AddToCart(p));
         }
         public void Loaded(MenuUC menuUC)
         {
             List<DISH> dishList = Data.Ins.DB.DISHES.ToList();
             menuUC.ViewListProducts.ItemsSource = dishList;
+            thisUC = menuUC;
+        }
+        public void AddToCart(ListViewItem listViewItem)
+        {
+            DISH selectedDish = listViewItem.DataContext as DISH;
+            thisUC.carts.Items.Add(selectedDish);
         }
         public void PreviousUC(MenuUC menuUC)
         {
@@ -35,5 +55,20 @@ namespace WeddingApp.ViewModels
         {
             MainVM.NextUC();
         }
+        //private long GetTotalPrice(ListView listView)
+        //{
+        //    long res = 0;
+        //    foreach (var lvi in FindVisualChildren<ListViewItem>(listView))
+        //    {
+        //        CART cart = lvi.DataContext as CART;
+        //        var checkBox = GetVisualChild<CheckBox>(lvi);
+        //        if (checkBox.IsChecked == true)
+        //        {
+        //            ép kiểu Giá = số lượng* giá sản phẩm *discount
+        //            res += (long)((Int32)cart.AMOUNT_ * (Int32)cart.PRODUCT.PRICE_ * (1 - (decimal)cart.PRODUCT.DISCOUNT_));
+        //        }
+        //    }
+        //    return res;
+        //}
     }
 }
