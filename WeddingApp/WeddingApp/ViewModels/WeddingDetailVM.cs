cad6822d0e3f672de47333b427f00979e435f23c
Điền Trương Kim <20520442@ms.uit.edu.vn>
2022-05-30 09:56:51 +0700

    WeddingDetail

---
+++ WeddingApp/WeddingApp/ViewModels/WeddingDetailVM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using WeddingApp.Views;
+using WeddingApp.Views.UserControls;
+using WeddingApp.Models;
+
+namespace WeddingApp.ViewModels
+{
+    internal class WeddingDetailVM : ViewModelBase
+    {
+        public ICommand SelectionChangedCommand { get; set; }
+        public ICommand LoadedCommand { get; set; }
+        public WeddingDetailVM()
+        {
+            LoadedCommand = new RelayCommand<WeddingDetailWindow>(p => true, p => Loaded(p));
+            SelectionChangedCommand = new RelayCommand<WeddingDetailWindow>(p => true, p => SwitchTab(p));
+        }
+        public void Loaded(WeddingDetailWindow weddingDetailWindow)
+        {
+            
+        }
+        public void SwitchTab(WeddingDetailWindow weddingDetailWindow)
+        {
+            switch (weddingDetailWindow.listViewTab.SelectedIndex)
+            {
+                case 0:
+                    DetailDishListUC detailDishListUC = new DetailDishListUC();
+                    weddingDetailWindow.listViewdish.Children.Clear();
+                    weddingDetailWindow.listViewdish.Children.Add(detailDishListUC);
+                    break;
+                case 1:
+                    DetailServiceListUC detailServiceListUC = new DetailServiceListUC();
+                    weddingDetailWindow.listViewdish.Children.Clear();
+                    weddingDetailWindow.listViewdish.Children.Add(detailServiceListUC);
+                    break;
+            }
+        }
+    }
+}
