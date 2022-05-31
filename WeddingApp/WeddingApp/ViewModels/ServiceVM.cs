7a23e5fee4e9d43f3a68ad49c4b262566261da18
Điền Trương Kim <20520442@ms.uit.edu.vn>
2022-05-31 19:24:12 +0700

    ServiceVM

---
+++ WeddingApp/WeddingApp/ViewModels/ServiceVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WeddingApp.Models;
+using WeddingApp.Views.UserControls;
+
+namespace WeddingApp.ViewModels
+{
+    internal class ServiceVM : ViewModelBase
+    {
+        public ICommand LoadedCommand { get; set; }
+        public ICommand PreviousUCCommand { get; set; }
+
+        public ServiceVM()
+        {
+            LoadedCommand = new RelayCommand<ServiceSelectionUC>(p => true, p => Loaded(p));
+            PreviousUCCommand = new RelayCommand<ServiceSelectionUC>(p => true, p => PreviousUC());
+        }
+        public void Loaded(ServiceSelectionUC serviceSelectionUC)
+        {
+            List<SERVICE> service = Data.Ins.DB.SERVICEs.ToList();
+            serviceSelectionUC.ServiceList.ItemsSource = service; 
+        }    
+        public void PreviousUC()
+        {
+            MainVM.PreviousUC();
+        }
+    }
+}
